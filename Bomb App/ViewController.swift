2e22119b8a501f5e7de50dfb5c8f710f1bd4cb68
jufot <jufot151@gmail.com>
2017-07-13 13:17:40 -0400

    Bomb-App
    Bomb App Project

--- Bomb App/ViewController.swift
+++ Bomb App/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    
+    
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +26,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueBomb.hidden = true
+        
+    }
+    @IBAction func hideRed(sender: AnyObject) {
+    
+        redBomb.hidden = true
+        
+    }
 
+    
 }
 
